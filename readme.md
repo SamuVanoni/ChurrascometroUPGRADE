35c74fa87eb45b52897bc3e660314b6181bd0de1
Samuel Oliveira <samu.prog00@gmail.com>
2023-04-21 16:29:30 -0300

    Churrascometro2.0

---
+++ readme.md
@@ -0,0 +1,5 @@
+Nesse projeto eu uso dos meus novos conhecimentos para melhorar o site do churrascometro que vim fazendo anteriormente.
+
+Unica coisa que não consegui fazer de maneira alguma: width: a cima de 800px eu queria que a img ficasse ao lado da tabela.
+
+Achei um footer padrão para minhas próximas páginas. :D
--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,9 @@
-/* Adultos totais, Adultos que vão beber, Crianças, Duração
-
-Carne, Frango, Linguiça, Pão com alho, Queijo
-Refrigerante, Álcool, Suco
-
-Duração 1 valor de 1h-3h
-Outro de 3h-6h, 6h-9h, 9h-12h+
-
-1 pessoa
-150g, 50g, 50g, 1, 2
-500ml, 4latas, 500ml
-*/
 
 let inputAdultosTT = document.getElementById("adultosTT");
 let inputAdultos = document.getElementById("adultos");
+let inputSucoAdultos = document.getElementById("sucoAdultos");
 let inputCriancas = document.getElementById("criancas");
-let inputSuco = document.getElementById("suco");
+let inputSucoCriancas = document.getElementById("sucoCriancas");
 let inputDuracao = document.getElementById("duracao");
 
 let resultado = document.getElementById("resultado");
@@ -23,8 +12,9 @@ function calcular(){
 
     let adultosTT = inputAdultosTT.value;
     let adultos = inputAdultos.value;
+    let sucoAdultos = inputSucoAdultos.value;
     let criancas = inputCriancas.value;
-    let suco = inputSuco.value;
+    let sucoCriancas = inputSucoCriancas.value;
     let duracao = inputDuracao.value;
 
     let carne = carnePP(duracao);
@@ -34,24 +24,25 @@ function calcular(){
     let queijo = queijoPP(duracao);
     let refrigerante = refriPP(duracao);
     let cerveja = cervejaPP(duracao);
+    let suco = sucoPP(duracao);
 
     let qtdTotalCarne = carne * adultosTT + (carne / 2 * criancas);
     let qtdTotalFrango = frango * adultosTT + (frango / 2 * criancas);
     let qtdTotalLinguica = linguica * adultosTT + (linguica / 2 * criancas);
     let qtdTotalPaoComAlho = paoComAlho * adultosTT + (paoComAlho / 2 * criancas);
     let qtdTotalQueijo = queijo * adultosTT + (queijo / 2 * criancas);
-    let qtdTotalRefri = refrigerante * adultosTT + (refrigerante / 2 * criancas);
+    let qtdTotalRefri = refrigerante * ((adultosTT - adultos) - sucoAdultos) + (refrigerante / 2 * (criancas - sucoCriancas));
     let qtdTotalCerveja = cerveja * adultos;
-    let qtdTotalSuco = 500 * suco;
+    let qtdTotalSuco = suco * sucoAdultos + (suco * sucoCriancas);
 
-    resultado.innerHTML = `<p>${qtdTotalCarne / 1000}Kg de carne</p>`
-    resultado.innerHTML += `<p>${qtdTotalFrango / 1000}Kg de frango</p>`
-    resultado.innerHTML += `<p>${qtdTotalLinguica / 1000}Kg de linguiça</p>`
-    resultado.innerHTML += `<p>${qtdTotalPaoComAlho / 1000}Kg de pão com alho</p>`
-    resultado.innerHTML += `<p>${qtdTotalQueijo / 4} pacotes de queijo</p>`
-    resultado.innerHTML += `<p>${qtdTotalRefri / 2000}garrafas de refri</p>`
-    resultado.innerHTML += `<p>${qtdTotalCerveja}latas de cerveja</p>`
-    resultado.innerHTML += `<p>${qtdTotalSuco / 1000}litros de suco</p>`
+    resultado.innerHTML = `<p> <img src="./images/bife.png" width="25px"> ${Math.ceil(qtdTotalCarne / 1000)}Kg de carne</p>`
+    resultado.innerHTML += `<p> <img src="./images/frango.png" width="25px"> ${Math.ceil(qtdTotalFrango / 1000)}Kg de frango</p>`
+    resultado.innerHTML += `<p> <img src="./images/linguica.png" width="25px"> ${Math.ceil(qtdTotalLinguica / 1000)}Kg de linguiça</p>`
+    resultado.innerHTML += `<p> <img src="./images/paoComAlho.png" width="25px"> ${Math.ceil(qtdTotalPaoComAlho / 4)} pacotes de pão com alho</p>`
+    resultado.innerHTML += `<p> <img src="./images/queijo.png" width="25px"> ${Math.ceil(qtdTotalQueijo / 4)} pacotes de queijo</p>`
+    resultado.innerHTML += `<p> <img src="./images/refri.png" width="25px"> ${Math.ceil(qtdTotalRefri / 2000)}garrafas de refri</p>`
+    resultado.innerHTML += `<p> <img src="./images/cerveja.png" width="25px"> ${qtdTotalCerveja}latas de cerveja</p>`
+    resultado.innerHTML += `<p> <img src="./images/suco.png" width="25px"> ${Math.ceil(qtdTotalSuco / 1000)}litros de suco</p>`
 
 }
 
@@ -71,30 +62,43 @@ function carnePP(duracao){
 function frangoPP(duracao){
 
     if(duracao <= 3){
-        return 50;
-    } else if (duracao > 3){
-        return 75;
-    } else if (duracao > 6){
         return 100;
+    } else if (duracao > 3){
+        return 150;
+    } else if (duracao > 6){
+        return 200;
     } else if (duracao > 9){
-        return 125;
+        return 250;
     }
 }
 
 function linguicaPP(duracao){
 
     if(duracao <= 3){
-        return 50;
-    } else if (duracao > 3){
-        return 75;
-    } else if (duracao > 6){
         return 100;
+    } else if (duracao > 3){
+        return 150;
+    } else if (duracao > 6){
+        return 200;
     } else if (duracao > 9){
-        return 125;
+        return 250;
     }
 }
 
 function paoComAlhoPP(duracao){
+
+    if(duracao <= 3){
+        return 1.5;
+    } else if (duracao > 3){
+        return 2.5;
+    } else if (duracao > 6){
+        return 3.5;
+    } else if (duracao > 9){
+        return 4.5;
+    }
+}
+
+function queijoPP(duracao){
 
     if(duracao <= 3){
         return 1;
@@ -104,19 +108,6 @@ function paoComAlhoPP(duracao){
         return 3;
     } else if (duracao > 9){
         return 4;
-    }
-}
-
-function queijoPP(duracao){
-
-    if(duracao <= 3){
-        return 2;
-    } else if (duracao > 3){
-        return 4;
-    } else if (duracao > 6){
-        return 6;
-    } else if (duracao > 9){
-        return 8;
     }
 }
 
@@ -144,4 +135,21 @@ function cervejaPP(duracao){
     } else if (duracao > 9){
         return 10;
     }
+}
+
+function sucoPP(duracao){
+
+    if(duracao <= 3){
+        return 500;
+    } else if (duracao > 3){
+        return 750;
+    } else if (duracao > 6){
+        return 1000;
+    } else if (duracao > 9){
+        return 1250;
+    }
+}
+
+function creditos(){
+    alert('<a href="https://www.flaticon.com/br/icones-gratis/bife" title="bife ícones">Bife ícones criados por Febrian Hidayat - Flaticon</a><a href="https://www.flaticon.com/free-icons/baguette" title="baguette icons">Baguette icons created by Freepik - Flaticon</a><a href="https://www.flaticon.com/free-icons/cheese" title="cheese icons">Cheese icons created by Freepik - Flaticon</a><a href="https://www.flaticon.com/free-icons/coke" title="coke icons">Coke icons created by Freepik - Flaticon</a><a href="https://www.flaticon.com/free-icons/drink" title="drink icons">Drink icons created by Freepik - Flaticon</a><a href="https://www.flaticon.com/free-icons/tequila" title="tequila icons">Tequila icons created by Freepik - Flaticon</a><a href="https://www.flaticon.com/free-icons/meat" title="meat icons">Meat icons created by Freepik - Flaticon</a>');
 }
